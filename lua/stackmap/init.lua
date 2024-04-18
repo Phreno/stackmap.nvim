31a203b03a81c5c9b15b42b7dcc16bfc14effbbe
phreno <losgoomy@gmail.com>
2024-04-18 21:12:32 +0200

    add a fuction to find a file

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,7 +1,12 @@
 local M = {}
 
-M.setup = function(opts)
-  print("Stackmap setup called with opts: ", opts)
+M.findFile = function(filename)
+  local path = vim.api.nvim_get_runtime_file(filename, false)
+  if path[1] == nil then
+    return nil
+  end
+
+  return path[1]
 end
 
 return M
