3e0e7a2c8a61109b5b4e21c682a368950b5c3168
phreno <losgoomy@gmail.com>
2024-04-18 21:16:32 +0200

    fix

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.findFile = function(filename)
-  local path = vim.api.nvim_get_runtime_file(filename, false)
-  if path[1] == nil then
-    return nil
-  end
+	local path = vim.api.nvim_get_runtime_file(filename, false)
+	if path[1] == nil then
+		return nil
+	end
 
-  print "Found file:" .. path[1]
-  return path[1]
+	print("Found file: " .. path[1])
+	return path[1]
 end
 
 return M
