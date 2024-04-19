40139d5c901ef385f4f602b9d26e017cc8a97bbd
phreno <losgoomy@gmail.com>
2024-04-19 21:48:29 +0200

    test

--- lua/stackmap/init.lua
+++ lua/stackmap/init.lua
@@ -1,17 +1,27 @@
 local M = {}
 
-M.find_file = function(name, directory)
-	local pfile = io.popen('find "' .. directory .. '" -type f')
-	if not pfile then
-		print("Error: Could not find file")
-		return
+vim.o.shell = "powershell"
+vim.o.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.o.shellquote = ""
+vim.o.shellxquote = ""
+
+local config = {
+	source_path = "~/foo",
+	output_path = "./bar",
+}
+
+M.deploy_teledis = function()
+	local ps_command = string.format(
+		'Copy-Item -Path  "%s\\*" -Destination "%s" -Recurse -Force',
+		config.source_path,
+		config.output_path
+	)
+	local result = vim.fn.system(ps_command)
+	if vim.v.shell_error ~= 0 then
+		print("Erreur lors de la copie des fichiers: " .. result)
+	else
+		print("Deploiement reussi.")
 	end
-	for filename in pfile:lines() do
-		if filename:match(name) then
-			print("Found: " .. filename)
-			return filename
-		end
-	end
-	pfile:close()
 end
+
 return M
